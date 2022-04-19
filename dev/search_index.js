d0af04e80fef6d11f3044e9a822e19fd8f734d14
Documenter.jl <documenter@juliadocs.github.io>
2022-04-19 15:28:34 +0000

    build based on 98a501f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#MyAwesomePackage.jl","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"","category":"section"},{"location":"","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"Documentation for MyAwesomePackage.jl","category":"page"},{"location":"","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"Some fix...","category":"page"}]
+[{"location":"#MyAwesomePackage.jl","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"","category":"section"},{"location":"","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"Documentation for MyAwesomePackage.jl","category":"page"},{"location":"","page":"MyAwesomePackage.jl","title":"MyAwesomePackage.jl","text":"Some fix... Further information","category":"page"}]
 }
